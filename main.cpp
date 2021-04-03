f3c6ba78148a4ee7b45b05604ab4880aeed2c194
Zhixuan Fang <zfang18@syr.edu>
2021-04-03 06:31:09 -0400

    update

--- main.cpp
+++ main.cpp
@@ -399,6 +399,57 @@ SinglyLinkedList::SinglyLinkedList(const initializer_list<int>& I)
 }
 
 //Binary tree
+class BSTIterator_iterative{
+private:
+	TreeNode* cur;
+	stack<TreeNode*> stk;
+public:
+	BSTIterator_iterative(TreeNode* root):cur(root){}
+
+	int next(){
+		while (cur != nullptr) {
+			stk.push(cur);
+			cur = cur->left;
+		}
+		cur = stk.top();
+		stk.pop();
+		int ret = cur->val;
+		cur = cur->right;
+		cout << cur->val << endl;
+		return ret;
+	}
+
+	bool hasNext() {
+		return cur != nullptr || !stk.empty();
+	}
+};
+class BSTIterator{
+private:
+	void inorder(TreeNode* root, vector<int>& res){
+		if(!root){
+			return;
+		}
+		inorder(root->left, res);
+		res.push_back(root->val);
+		inorder(root->right, res);
+	}
+	vector<int> inorderTraversal(TreeNode* root){
+		vector<int> res;
+		inorder(root, res);
+		return res;
+	}
+	vector<int> arr;
+	int idx;
+public:
+	BSTIterator(TreeNode* root):idx(0),arr(inorderTraversal(root)){}
+
+	int next(){
+		return arr[idx++];
+	}
+	bool hasNext(){
+		return (idx<arr.size());
+	}
+};
 class diameter_of_binary_tree{
 	int ans;
 	int depth(TreeNode* rt){
@@ -937,6 +988,22 @@ public:
 		return dp[n];
 	}
 
+};
+class find_min_in_rotated_array{
+public:
+	int findMin(vector<int>& nums){
+		int left = 0;
+		int right = nums.size()-1;
+		while(left<right){
+			int mid = left+(right-left)/2;
+			if(nums[mid]>nums[right]){
+				left = mid+1;
+			}else{
+				right = mid;
+			}
+		}
+		return nums[left];
+	}
 };
 class first_last_pos_inArray{
 public:
@@ -1843,7 +1910,67 @@ public:
 		return result;
 	}
 };
-
+class AutocompleteSystem {
+public:
+	string cur;
+	//当前输入的查询字符串
+	int idx;
+	//当前输入查询字符在查询字符串的下标
+	vector<string> res;
+	//目前所有可能的补全结果，按题目要求排序（热度递减、字典序递增）
+	unordered_map<char, map<string, int>> cnt;
+	//以各个字母开头的所有字符串的历史查询次数
+	static bool cmp(pair<string, int>& a, pair<string, int>& b){//排序函数
+		if(a.second != b.second)
+			return a.second > b.second;
+		else
+			return a.first < b.first;
+	}
+	AutocompleteSystem(vector<string>& sentences, vector<int>& times) {
+		cur = "";
+		idx = 0;
+		res.clear();
+		cnt.clear();
+		for(int i = 0; i < sentences.size(); i++)
+			cnt[sentences[i][0]][sentences[i]] = times[i];
+	}
+	vector<string> input(char c) {
+		if(c == '#'){//本此查询完毕，更新cnt，清空缓存量
+			cnt[cur[0]][cur]++;
+			cur = "";
+			idx = 0;
+			res.clear();
+			return res;
+		}
+		if(idx == 0){//输入查询字符串的第一个字母
+			cur += c;
+			idx++;
+			//找到所有可能的补全结果
+			vector<pair<string, int>> temp;
+			for(auto it = cnt[c].begin(); it != cnt[c].end(); it++)
+				temp.push_back(*it);
+			sort(temp.begin(), temp.end(), cmp);//排序
+			for(auto it = temp.begin(); it != temp.end(); it++)
+				res.push_back(it->first);
+		}
+		else{
+			auto it = res.begin();
+			while(it != res.end()){//去掉res中不可能是补全结果的单词
+				if((*it).size() <= idx || (*it)[idx] != c)
+					res.erase(it);
+				else
+					it++;
+			}
+			cur += c;
+			idx++;
+		}
+		//返回res中的前三个
+		if(res.size() < 3)
+			return res;
+		else
+			return vector<string>(res.begin(), res.begin()+3);
+	}
+};
 class two_sum {//twosum
 public:
 	vector<int> twosum(vector<int>& nums, int target)
@@ -4019,6 +4146,21 @@ int main()
 
 	first_last_pos_inArray search_range;
 	vector<int> search_range_vec{5,7,7,8,8,10};
-	cout << search_range.searchRange(search_range_vec, 8);
+	cout << search_range.searchRange(search_range_vec, 8)<<endl;
 
+	cout << "BSTIterator_: ";
+	TreeNode* Bst_Root = new TreeNode(7);
+	TreeNode* Bst_Root_Left = new TreeNode(3);
+	TreeNode* Bst_Root_Right = new TreeNode(15);
+	TreeNode* Bst_Root_Right_Left = new TreeNode(9);
+	TreeNode* Bst_Root_Right_Right = new TreeNode(20);
+	Bst_Root->left = Bst_Root_Left;
+	Bst_Root->right = Bst_Root_Right;
+	Bst_Root_Right->left = Bst_Root_Right_Left;
+	Bst_Root_Right->right = Bst_Root_Right_Right;
+	BSTIterator* bst_iterator = new BSTIterator(Bst_Root);
+	cout<<bst_iterator->next()<<endl;
+
+	//BSTIterator_iterative* bst_iterator_iterative = new BSTIterator_iterative(Bst_Root);
+	//cout << bst_iterator_iterative->next() << endl;
 }
